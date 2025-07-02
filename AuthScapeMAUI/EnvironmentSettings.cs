67cee3696f2acbf26c377bf8856f765e6e85fddf
Brandon Zuech <Brandonzuech@hotmail.com>
2025-07-02 19:25:59 -0400

    env settings

---
+++ AuthScapeMAUI/EnvironmentSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthScapeMAUI
+{
+    public class EnvironmentSettings
+    {
+        const string ClientId = "";
+        const string Secret = "";
+        const string BaseAPI = "https://api.authscape.com";
+        const string RedirectUri = "https://authscape.com/callback";
+        const string BaseIDP = "";
+        const string CompanyName = "AuthScape";
+    }
+}
